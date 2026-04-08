ebfd337722785c31f198a8ee21ec7daac78e48c9
PinzyOffc <pinzyxiiterz223@gmail.com>
2026-04-09 06:14:06 +0700

    Update justinoffc.js

--- justinoffc.js
+++ justinoffc.js
@@ -147,6 +147,9 @@ async function streamToBuffer(stream) {
 const fetch = require("node-fetch")
 const FormData = require("form-data")
 
+const fetch = require("node-fetch")
+const FormData = require("form-data")
+
 async function uploadImage(buffer) {
     try {
         const form = new FormData()
